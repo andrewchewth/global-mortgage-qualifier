cfdcff6ce48d019902486cc0a587db423e6857a6
Andrew Chew <andrewchewth@gmail.com>
2025-03-17 19:00:56 +0800

    Fix ESLint configuration and dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,11 @@
 module.exports = {
   root: true,
-  extends: ['next/core-web-vitals'],
+  extends: [
+    'next/core-web-vitals',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
   rules: {
     '@typescript-eslint/no-explicit-any': 'off',
     'react/no-unescaped-entities': 'off',
